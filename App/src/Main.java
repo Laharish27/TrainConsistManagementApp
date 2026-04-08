1a079c2bfedb680872f00f3e0dd07623f1435ff6
Laharish27 <laharishkondaveeti2007@gmail.com>
2026-04-08 15:02:27 +0530

    UC9

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,1 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
 
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + "(" + capacity + ")";
+    }
+}
+
+public class UC9_GroupBogies {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = Arrays.asList(
+                new Bogie("Sleeper", 72),
+                new Bogie("AC Chair", 60),
+                new Bogie("Sleeper", 80),
+                new Bogie("First Class", 40)
+        );
+
+        Map<String, List<Bogie>> grouped =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.name));
+
+        grouped.forEach((type, list) -> {
+            System.out.println(type + " : " + list);
+        });
+    }
+}
